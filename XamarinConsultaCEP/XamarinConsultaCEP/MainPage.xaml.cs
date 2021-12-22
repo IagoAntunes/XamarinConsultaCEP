3582561cd262023c86261799c77bb45bca536ca4
iagoa <iagoantunes.f@gmail.com>
2021-12-22 11:38:53 -0300

    Adicionar arquivos de projeto.

---
+++ XamarinConsultaCEP/XamarinConsultaCEP/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinConsultaCEP.Servico.Modelo;
+using XamarinConsultaCEP.Servico;
+
+namespace XamarinConsultaCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Botao.Clicked += BuscarCEP;
+
+        }
+
+        private void BuscarCEP(object sender, EventArgs e)
+        {
+            string cep = CEP.Text.Trim();
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCepServico.BuscarEnderedoViaCep(cep);
+                    if (end != null)
+                        Resultado.Text = string.Format("Endereco: \n{0} \n{1} \n{2} \n{3}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    else
+                        DisplayAlert("Erro", "Endereco nao encotnrado", "Ok");
+                }catch(Exception ex)
+                {
+                    DisplayAlert("ERRO", ex.Message, "Ok");
+                }
+            }
+        }
+        private bool isValidCep(string cep)
+        {
+            Boolean valido = true;
+
+            if(cep.Length != 8)
+            {
+                //ERRO
+                DisplayAlert("ERRO", "CEP deve ter 8 caracteres.", "Ok");
+                valido = false;
+            }
+            int NovoCep = 0;
+
+            if (!int.TryParse(cep, out NovoCep))
+            {
+                //ERRO
+                valido = false;
+                DisplayAlert("ERRO", "CEP deve ser composto apenas por numeros.", "Ok");
+            }
+
+            return valido;
+        }
+    }
+}
---
+++ XamarinConsultaCEP/XamarinConsultaCEP/Servico/ViaCepServico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using XamarinConsultaCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace XamarinConsultaCEP.Servico
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderedoViaCep(string cep)
+        {
+            string NovoEnderecoUrl = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoUrl);//Recebe dados do site
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);//Converter JSON para objeto
+            if(end.cep == null)
+                return null;
+
+            return end;
+        }
+        
+    }
+}
